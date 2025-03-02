5fb80cda168a5bb277edc96067e97d0eb5486f7e
Tomzaza-joint <tommac5@ktu.lt>
2025-03-02 15:01:51 +0200

    Added music and sfx

---
+++ CrazyFarmerGame/Assets/Audiomanager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Audiomanager : MonoBehaviour
+{
+    [Header("--------- Audio Source ---------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------- Audio Clip ---------")]
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip wallTouch;
+}
