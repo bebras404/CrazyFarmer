858cadce7cde3c92cd8f538d78b5c5d8ea680279
bebras404 <adodan@ktu.lt>
2025-03-02 13:47:16 +0200

    Fixed player damage

--- CrazyFarmerGame/Assets/AI/Scripts/AIDamageDealing.cs
+++ CrazyFarmerGame/Assets/AI/Scripts/AIDamageDealing.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class AIDamageDealing : MonoBehaviour
@@ -14,10 +15,11 @@ public class AIDamageDealing : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Touched!");
         GetComponent<Collider2D>().enabled = false;
         Enemy.GetComponent<SpriteRenderer>().flipY = true;
         Enemy.GetComponent<Collider2D>().enabled = false;
-        Vector3 movement = new Vector3(Random.Range(40, 70), Random.Range(-40, 40), 0f);
+        Vector3 movement = new Vector3(UnityEngine.Random.Range(40, 70), UnityEngine.Random.Range(-40, 40), 0f);
         Enemy.transform.position = Enemy.transform.position + movement * Time.deltaTime;
     }
 
