e29f85ef83a02647595a923de51ca0713f226310
bebras404 <adodan@ktu.lt>
2025-03-02 16:40:59 +0200

    Fixed sounds and SFX

--- CrazyFarmerGame/Assets/Sounds/Audiomanager.cs
+++ CrazyFarmerGame/Assets/Sounds/Audiomanager.cs
@@ -11,11 +11,24 @@ public class Audiomanager : MonoBehaviour
     public AudioClip background;
     public AudioClip death;
     public AudioClip wallTouch;
+    public GameObject player;
+
+    private bool hasPlayedDeathSFX = false;
 
     private void Start()
     {
         musicSource.clip = background;
         musicSource.Play();
         musicSource.volume = 0.1f;
+    }
+
+    private void Update()
+    {
+        if (player.GetComponent<PlayerHealth>().health <= 0 && !hasPlayedDeathSFX)
+        {
+            SFXSource.PlayOneShot(death);
+            musicSource.Stop();
+            hasPlayedDeathSFX = true;
+        }
     }
 }
