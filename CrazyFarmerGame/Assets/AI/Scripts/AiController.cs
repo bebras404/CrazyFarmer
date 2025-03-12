ce191e47f1e6b76a0b7f983c1c91046614d33d8a
bebras404 <adodan@ktu.lt>
2025-03-12 17:00:39 +0200

    Added flying AI

--- CrazyFarmerGame/Assets/AI/Scripts/AiController.cs
+++ CrazyFarmerGame/Assets/AI/Scripts/AiController.cs
@@ -1,5 +1,5 @@
+﻿using UnityEngine;
 using System.Collections;
-using UnityEngine;
 
 public class AiController : MonoBehaviour
 {
@@ -81,38 +81,38 @@ public class AiController : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
+{
+    // Ignore collisions with the head layer
+    if (collision.gameObject.layer == LayerMask.NameToLayer("Head"))
     {
-        // Ignore collisions with the head layer
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Head"))
-        {
-            return;
-        }
-
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            isTouchingPlayer = true;
-            if (damageCoroutine == null)
-            {
-                damageCoroutine = StartCoroutine(DealDamage());
-            }
-        }
+        return;
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    if (collision.gameObject.CompareTag("Player"))
     {
-
-
-        if (collision.gameObject.CompareTag("Player"))
+        isTouchingPlayer = true;
+        if (damageCoroutine == null)
         {
-            isTouchingPlayer = false;
-
-            if (damageCoroutine != null)
-            {
-                StopCoroutine(damageCoroutine);
-                damageCoroutine = null;
-            }
+            damageCoroutine = StartCoroutine(DealDamage());
         }
     }
+}
+
+private void OnCollisionExit2D(Collision2D collision)
+{
+
+
+    if (collision.gameObject.CompareTag("Player"))
+    {
+        isTouchingPlayer = false;
+
+        if (damageCoroutine != null)
+        {
+            StopCoroutine(damageCoroutine);
+            damageCoroutine = null;
+        }
+    }
+}
 
 
     private IEnumerator DealDamage()
