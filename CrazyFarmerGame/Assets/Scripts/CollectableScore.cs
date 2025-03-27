a4794e52a9804b8a09e732451bcfc83e5be48f4a
jacobobos <jokstu@ktu.lt>
2025-03-27 18:15:54 +0200

    Added collectables that add score when picked up

---
+++ CrazyFarmerGame/Assets/Scripts/CollectableScore.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CollectableScore : MonoBehaviour
+{
+    public ScoreManager sm;
+    public int ScoreToAdd = 200;
+    public float floatSpeed = 0;
+    public float floatHeight = 0;
+    private Vector3 startPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    public void SetPlayer(GameObject obj)
+    {
+        Transform scoreMTransform = obj.transform.Find("ScoreM");
+        if (scoreMTransform != null)
+        {
+            sm = scoreMTransform.GetComponent<ScoreManager>();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            sm.AddScore(ScoreToAdd);
+        }
+        Destroy(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+}
