b3e89d12fb0a3bf41f187a1f28c9f88bc710d678
bebras404 <adodan@ktu.lt>
2025-03-30 20:57:35 +0300

    Revert "Merge"
    This reverts commit 0cfc87bd40fadcf480a160b562aef624dd9bc6b0, reversing
    changes made to 616cca55b2562ec670654c4995a400224d10eead.

--- CrazyFarmerGame/Assets/Meniu/MainMeniuScript.cs
+++ CrazyFarmerGame/Assets/Meniu/MainMeniuScript.cs
@@ -3,28 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMeniuScript : MonoBehaviour
 {
-    private LevelLoader levelLoader;
-
-    void Start()
-    {
-        levelLoader = FindFirstObjectByType<LevelLoader>(); // Correct method
-    }
-
     public void PlayButton()
     {
-        if (levelLoader != null)
-        {
-            levelLoader.LoadScene(3); // Load Main Game (scene 3)
-        }
-        else
-        {
-            Debug.LogError("LevelLoader not found! Make sure it's in the scene.");
-            SceneManager.LoadScene(3); // Fallback in case LevelLoader is missing
-        }
+        SceneManager.LoadSceneAsync(3);
     }
 
     public void SettingsButton()
     {
-        SceneManager.LoadScene(1); // Load Settings (scene 1)
+        SceneManager.LoadSceneAsync(1);
     }
+
+
 }
