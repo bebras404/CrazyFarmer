a347a68931ebbbb265ea8ddfe2e50a51ec0e7edc
Damke24 <dombar7@ktu.lt>
2025-03-31 02:09:11 +0200

    .

--- CrazyFarmerGame/Assets/CoinManager.cs
+++ CrazyFarmerGame/Assets/CoinManager.cs
@@ -1,10 +1,12 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CoinManager : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI coinText;
+
     public int coinCount;
-    public Text coinText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
